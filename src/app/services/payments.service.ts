e3cb20756007fa11367883a38c4eb0e8d1eff3bc
‎WM <91469450+sys-arch@users.noreply.github.com>
2025-05-12 23:16:10 +0200

    Añadido confirmar pago en servicio y component

--- src/app/services/payments.service.ts
+++ src/app/services/payments.service.ts
@@ -1,21 +1,35 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class PaymentsService {
 
-  constructor(private client : HttpClient) { }
+  constructor(private client: HttpClient) {}
 
-  prepay() : Observable<Object> {
-return this.client.get("http://localhost:8003/payments/prepay", {
-  withCredentials: true,
-  observe: "response",
-  responseType: "text"
-});
-}
+  prepay(): Observable<Object> {
+    return this.client.get("http://localhost:8003/payments/prepay", {
+      withCredentials: true,
+      observe: "response",
+      responseType: "text"
+    });
+  }
+
+  confirm(token: string, credits: number): Observable<Object> {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    });
+
+    const body = { credits };
+
+    return this.client.put("http://localhost:8003/payments/confirm", body, {
+      headers,
+      withCredentials: true,
+      observe: "response",
+      responseType: "text"
+    });
+  }
 }
